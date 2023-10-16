13eb57639b350e4a259f3f789c01c81983413c6d
oalbers <olgerik.albers@gmail.com>
2023-10-16 17:05:33 +0200

    Update to .net 6

--- WSDLMerge/Properties/AssemblyInfo.cs
+++ WSDLMerge/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle ( "WSDLMerge" )]
-[assembly: AssemblyDescription ( "" )]
-[assembly: AssemblyConfiguration ( "" )]
-[assembly: AssemblyCompany ( "Lenard Gunda" )]
-[assembly: AssemblyProduct ( "WSDLMerge" )]
-[assembly: AssemblyCopyright ( "Copyright © Lenard Gunda 2011-2013" )]
 [assembly: AssemblyTrademark ( "" )]
 [assembly: AssemblyCulture ( "" )]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid ( "88a0e04b-c7cd-4036-aa04-cbb89bb2086e" )]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ( "1.2.0.0" )]
-[assembly: AssemblyFileVersion ( "1.2.0.0" )]
